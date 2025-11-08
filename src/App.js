1d0c4591c84d02a60df5599d9a938690bed1ccf2
sakshit-t <sakshi.t@webedgesol.com>
2025-11-08 17:03:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App font-bold text-3xl text-red-800 padding-10px"> 
+          welcome to the netflix and whole differently netflixgpt
+    </div>
+  );
+}
+
+export default App;
